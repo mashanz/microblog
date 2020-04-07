3263dc5634fe22cfc0cd8166572054de65c396f4
Hanjara CA (HANS) <mangatkk@gmail.com>
2020-04-07 19:30:55 +0700

    avatar and edit user

--- requerement.sh
+++ requerement.sh
@@ -1,5 +1,10 @@
 conda install flask
-conda install flask-wtf
-conda install flask-sqlalchemy
 conda install flask-login
+conda install flask-openid
+conda install flask-sqlalchemy
+conda install flask-wtf
+conda install flask-babel
+conda install coverage
+conda install -c conda-forge sqlalchemy-migrate
+conda install -c conda-forge flask-mail
 conda install -c conda-forge flask-migrate
