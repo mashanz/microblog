ebc8e8c45805a2fa1c8a73fc38d5c62b5dd8c086
Hanjara CA (HANS) <mangatkk@gmail.com>
2020-04-10 15:56:10 +0700

    add bootstrap template

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,6 +15,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login = LoginManager(app)
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
--- requerement.sh
+++ requerement.sh
@@ -5,6 +5,7 @@ conda install flask-sqlalchemy
 conda install flask-wtf
 conda install flask-babel
 conda install coverage
+conda install -c conda-forge flask-bootstrap
 conda install -c conda-forge sqlalchemy-migrate
 conda install -c conda-forge flask-mail
 conda install -c conda-forge flask-migrate
